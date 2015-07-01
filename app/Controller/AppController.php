18bad0d0dc3b2c6f5ee14ad3e10344dcd63015c7
higashiguchi_kazuki@yahoo.co.jp <study@ip-172-31-28-89.ap-northeast-1.compute.internal>
2015-07-01 13:00:12 +0000

    vol/01 complete

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -19,7 +19,7 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-App::uses('Controller', 'Controller');
+App::uses('AppController', 'Controller');
 
 /**
  * Application Controller
@@ -31,5 +31,8 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-    public $components = array('DebugKit.Toolbar');
+    public $components = array(
+	'DebugKit.Toolbar',
+	'RequestHandler'
+    );
 }
